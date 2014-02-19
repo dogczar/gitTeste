5f4f812b794010fa335b68f90c62380e08a049e7
Usuário Padrão <maximilianobatista@iMac-de-Maximiliano-Batista.local>
2014-02-18 23:54:36 -0300

    Teste 1

---
+++ TesteMacRenan/TesteMacRenan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TesteMacRenan
+//
+//  Created by Usuário Padrão on 18/02/14.
+//  Copyright (c) 2014 Renan Junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TesteMacRenan/TesteMacRenan/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TesteMacRenan
+//
+//  Created by Usuário Padrão on 18/02/14.
+//  Copyright (c) 2014 Renan Junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
